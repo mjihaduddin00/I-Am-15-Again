e91c1f8eb8e6faae855ea0eaf0c62623df6b50fb
Mohammad Jihad Uddin <mjihaduddin00@outlook.com>
2021-03-01 14:47:13 -0600

    Main File
    I am committing the main file that the application is comprised of (Java File).

---
+++ Solution.java
@@ -0,0 +1,13 @@
+/* 
+I'm 15 again!
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String name = "Amigo";
+        int age = 15;
+        int money = 500;
+        int weight = 70;
+    }
+}
